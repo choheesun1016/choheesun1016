a319891385284dbfbe866c71f711668f66c8522f
Heesun Cho <chs7327@naver.com>
2022-06-05 10:58:26 +0900

    first commit

---
+++ app/src/main/java/com/example/termproject/MemoStudy.java
@@ -0,0 +1,22 @@
+package com.example.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MemoStudy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo_study);
+
+
+    }
+}
